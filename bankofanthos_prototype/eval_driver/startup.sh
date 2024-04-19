cdf545789a57608b0899b4084103e9adee57d93e
Kexin Zhu <zhukexin@google.com>
2024-04-18 17:13:44 -0700

    add auto explain in queries

--- bankofanthos_prototype/eval_driver/startup.sh
+++ bankofanthos_prototype/eval_driver/startup.sh
@@ -1,22 +1,56 @@
+# set -euo pipefail
+
+# main() {
+    
+# docker stop $(docker ps -q -f "name=postgres") 
+
+# docker run \
+#     --rm \
+#     --detach \
+#     --name postgres \
+#     --env POSTGRES_PASSWORD=password \
+#     --volume="$(realpath postgres.sh):/app/postgres.sh" \
+#     --volume="$(realpath postgresdb.sql):/app/postgresdb.sql" \
+#     --volume="$(realpath accountsdb.sql):/app/accountsdb.sql" \
+#     --volume="$(realpath 1_create_transactions.sh):/app/1_create_transactions.sh" \
+#     --publish 127.0.0.1:5432:5432 \
+#     postgres:15
+
+# sleep 2
+
+# # establish database
+# docker exec -it postgres /app/postgres.sh
+
+# openssl genrsa -out jwtRS256.key 4096
+# openssl rsa -in jwtRS256.key -outform PEM -pubout -out jwtRS256.key.pub
+# mkdir -p /tmp/.ssh
+# mv jwtRS256.key jwtRS256.key.pub /tmp/.ssh
+# }
+
+# main "$@"
+
+
 set -euo pipefail
 
 main() {
     
-docker stop $(docker ps -q -f "name=postgres") 
+docker stop postgres 
 
+# Run the Postgres instance.
 docker run \
     --rm \
     --detach \
     --name postgres \
-    --env POSTGRES_PASSWORD=password \
-    --volume="$(realpath postgres.sh):/app/postgres.sh" \
-    --volume="$(realpath postgresdb.sql):/app/postgresdb.sql" \
-    --volume="$(realpath accountsdb.sql):/app/accountsdb.sql" \
-    --volume="$(realpath 1_create_transactions.sh):/app/1_create_transactions.sh" \
+    --env POSTGRES_PASSWORD=password\
+    --volume="/usr/local/google/home/zhukexin/database/bankofanthos_prototype/bankofanthos/postgres.sh:/app/postgres.sh" \
+    --volume="/usr/local/google/home/zhukexin/database/bankofanthos_prototype/bankofanthos/postgresdb.sql:/app/postgresdb.sql" \
+    --volume="/usr/local/google/home/zhukexin/database/bankofanthos_prototype/bankofanthos/accountsdb.sql:/app/accountsdb.sql" \
+    --volume="/usr/local/google/home/zhukexin/database/bankofanthos_prototype/bankofanthos/1_create_transactions.sh:/app/1_create_transactions.sh" \
+    --volume="/usr/local/google/home/zhukexin/database/bankofanthos_prototype/eval_driver/postgresql.conf:/etc/postgres/postgresql.conf" \
     --publish 127.0.0.1:5432:5432 \
-    postgres:15
-
-sleep 2
+    postgres:15 \
+    -c log_statement=all -c config_file=/etc/postgres/postgresql.conf
+sleep 5
 
 # establish database
 docker exec -it postgres /app/postgres.sh
@@ -26,5 +60,3 @@ openssl rsa -in jwtRS256.key -outform PEM -pubout -out jwtRS256.key.pub
 mkdir -p /tmp/.ssh
 mv jwtRS256.key jwtRS256.key.pub /tmp/.ssh
 }
-
-main "$@"
